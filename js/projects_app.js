8d5c1f999a007395ef821547349083579c5cabdd
bernicetoh <bernicetoh2003@gmail.com>
2023-02-21 15:28:58 +0800

    Create scroll to top button in projects.html

--- js/projects_app.js
+++ js/projects_app.js
@@ -5,7 +5,16 @@ tl.to(".menu", {
   opacity: 1,
   y: 0,
   duration: 0.6,
-});
+}).to(
+  ".topBtn-container",
+  {
+    "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+    opacity: 1,
+    y: 0,
+    duration: 0.6,
+  },
+  "-=1"
+);
 
 /*--------------------
 Vars
@@ -150,3 +159,25 @@ window.smoothScroll = function (target) {
   // start scrolling
   scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 };
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
